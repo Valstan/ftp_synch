0986b33148ab3c262ae24253cfb38aece1529158
Валентин <123qwemmm@mail.ru>
2021-06-29 17:40:00 +0300

    update script backup bases postopus

--- backup_bases_postopus.py
+++ backup_bases_postopus.py
@@ -1,14 +1,21 @@
 import paramiko
 from lopes import ftp_synch_host, ftp_synch_port, ftp_synch_username, ftp_synch_password
 
+import os
+import datetime
 
-local_in_file = "Message_БР_МИ.xml"
-remote_in_file = "/root/ftp_synch/Message_БР_МИ.xml"
+local_dir = datetime.datetime.now().strftime('%m-%d_%H')
+os.makedirs(local_dir)
+
+filenames = "dbase.json", "mbase.json", "post_me_base.json"
+remote_dir = "/root/postopus/bases/"
 
 ssh_client = paramiko.SSHClient()
 ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 ssh_client.connect(ftp_synch_host, ftp_synch_port, ftp_synch_username, ftp_synch_password)
-
 ftp_client = ssh_client.open_sftp()
-ftp_client.get(remote_in_file, local_in_file)
+
+for fname in filenames:
+    ftp_client.get(remote_dir + fname, local_dir + fname)
+
 ftp_client.close()
